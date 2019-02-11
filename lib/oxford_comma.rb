ce4224698befcb6501fbce6c36bb94f3fbf87eab
Jaela Wesley <jcwesley93@gmail.com>
2019-02-11 02:47:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+if array.length == 1
+  array.join
+elsif array.length == 2
+  array.join(" and ")
+elsif array.length == 3
+  array.insert(3, "and ")
+  new_tail = array(3) + array(4)
+  array.delete_at(3)
+  array.delete_at(4)
+  array.push(new_tail)
+  array.join(", ")
+else array.length > 3
+end
 end
